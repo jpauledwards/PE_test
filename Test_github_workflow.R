f4bb55b7e38b7d001d99532ae43aeae6ec801554
jpauledwards <Paul.Edwards@dairynz.co.nz>
2020-10-20 11:21:59 +1300

    testing changes work

--- Test_github_workflow.R
+++ Test_github_workflow.R
@@ -2,3 +2,8 @@
 library(usethis)
 use_git_config(user.name = "jpauledwards", user.email = "Paul.Edwards@dairynz.co.nz")
 usethis::use_git()
+
+usethis::browse_github_pat()
+usethis::edit_r_environ()
+#GITHUB_PAT=d51e3b4f661ba45952766a90cbee2b6d5116c982
+
